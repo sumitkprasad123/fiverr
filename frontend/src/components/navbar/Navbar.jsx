22577d852bb2306109c3d95ce1cc04c11333fb57
sumit <sumitkprasad123@gmail.com>
2023-11-16 23:02:00 +0530

    navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,9 +1,99 @@
-// import React from 'react'
+import {useState,useEffect} from 'react'
+import { Link, useLocation } from "react-router-dom"
 import "./Navbar.scss"
 
 const Navbar = () => {
+  const [active,setActive] = useState(false);
+  const [open,setOpen] = useState(false);
+  const {pathname} = useLocation()
+ 
+  const isActive = () => {
+      window.scrollY > 0 ?setActive(true):setActive(false)
+  }
+  useEffect(() => {
+    window.addEventListener("scroll",isActive)
+  },[])
+
+  const currentUser = {
+      id:1,
+      username:"Jhon Doe ",
+      isSeller:true
+  }
+//  const currentUser = null
+  
   return (
-    <div className='navbar'>Navbar</div>
+    <div className={active || pathname !== "/"?'navbar active':'navbar'}>
+       <div className="container">
+          <div className="logo">
+            <Link to="/" className='link'> 
+              <span className="text">fiverr</span>
+              <span className="dot">.</span>
+            </Link>
+          </div>
+          <div className="links">
+              <span>Fiverr Business</span>
+              <span>Explore</span>
+              <span>English</span>
+              <span>Sign In</span>
+              {!currentUser?.isSeller && <span>Become a seller</span>}
+              {!currentUser && <button>Join</button>}
+              {currentUser && (
+                <div className="user" onClick={() => setOpen(!open)}>
+                   <img src="https://avatars.githubusercontent.com/u/107460715?v=4" alt="" />
+                   <span>{currentUser?.username}</span>
+                  { open && <div className="option">
+                      {
+                        currentUser?.isSeller && (
+                          <>
+                            <Link className='link' to="/myGigs">Gigs</Link>
+                            <Link className='link' to="/add">Add New Gig</Link>
+                          </>
+                        )
+                      }
+                      <Link className='link' to="/order">Order</Link>
+                      <Link className='link' to="/messages">Messages</Link>
+                      <Link className='link' to="/">Logout</Link>
+                   </div>}
+                </div>
+              )}
+          </div>   
+       </div>
+
+       {active || pathname !== "/" &&<>
+       <hr />
+          <div className="menu">
+            <Link className="link menuLink" to="/">
+              Graphics & Design
+            </Link>
+            <Link className="link menuLink" to="/">
+              Video & Animation
+            </Link>
+            <Link className="link menuLink" to="/">
+              Writing & Translation
+            </Link>
+            <Link className="link menuLink" to="/">
+              AI Services
+            </Link>
+            <Link className="link menuLink" to="/">
+              Digital Marketing
+            </Link>
+            <Link className="link menuLink" to="/">
+              Music & Audio
+            </Link>
+            <Link className="link menuLink" to="/">
+              Programming & Tech
+            </Link>
+            <Link className="link menuLink" to="/">
+              Business
+            </Link>
+            <Link className="link menuLink" to="/">
+              Lifestyle
+            </Link>
+          </div>
+          <hr />
+       </>
+       }
+    </div>
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,23 @@
 // import React from 'react'
-import "./home.scss"
+import CatCard from "../../components/catCard/CatCard"
+import Featured from "../../components/featured/Featured"
+import Slide from "../../components/slide/Slide"
+import TrustedBy from "../../components/trustedBy/TrustedBy"
+import "./Home.scss"
+import {cards} from "../../data"
+
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+       <Featured />
+       <TrustedBy />
+       <Slide slidesToShow={5} arrowsScroll={5}>
+          {cards.map((card) => (
+            <CatCard card={card} key={card.id}/>
+          ))}
+       </Slide>
+    </div>
   )
 }
 
