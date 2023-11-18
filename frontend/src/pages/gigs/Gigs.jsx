37a7f87b4caa65ee5d2a95d1e6c3c4112cd6ca99
sumit <sumitkprasad123@gmail.com>
2023-11-18 12:00:31 +0530

    gigs and gigsCard file

--- frontend/src/pages/gigs/Gigs.jsx
+++ frontend/src/pages/gigs/Gigs.jsx
@@ -1,9 +1,60 @@
 // import React from 'react'
 import "./Gigs.scss"
+import {useState} from "react"
+import GigCard from "../../components/gigCard/GigCard"
+import {gigs} from "../../data"
 
 const Gigs = () => {
+  const [sort, setSort] = useState("sales")
+  const [open, setOpen] = useState(false)
+
+
+  const reSort = (type) => {
+    setSort(type)
+    setOpen(false)
+  };
+
   return (
-    <div className='gigs'>Gigs</div>
+    <div className='gigs'>
+        <div className="container">
+          <span className="breadcrumbs"> Liverr &gt; Graphics & Design &gt; </span>
+          <h1>AI Artists</h1>
+          <p>
+            Explore the boundaries of art and technology with Liverr&apos;s AI artists
+          </p>
+          <div className="menu">
+            <div className="left">
+                <span>Budget</span>
+                <input type="number" placeholder="min" />
+                <input type="number" placeholder="max" />
+                <button>Apply</button>
+            </div>
+            <div className="right">
+               <span className="sortBy">Sort by</span>
+               <span className="sortType">
+                 {sort === "sales" ? "Best Selling" : "Newest"}
+               </span>
+               <img src="./img/down.png" alt="" onClick={() => setOpen(!open) } />
+               {open && (
+                <div className="rightMenu">
+                   {sort === "sales" ? (
+                     <span onClick={() => reSort("createdAt")}>Newest</span>
+                   ):(
+                     <span onClick={() => reSort("sales")}>Best Selling</span>
+                   )}
+                   <span onClick={() => reSort("sales")}>Popular</span>
+                </div>
+               )}
+            </div>
+          </div>
+          {/* all gigs cards */}
+          <div className="cards">
+             {gigs.map((gig) => (
+              <GigCard key={gig.id} item={gig} />
+             ))}
+          </div>
+        </div>
+    </div>
   )
 }
 
